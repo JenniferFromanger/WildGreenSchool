943f87b1c19010e2fdb72fee2f6287e3e9ef2285
narudb <benjam.d@gmail.com>
2020-03-19 12:21:29 +0100

    ajout du bouton back top sur homepage et formations

--- js/btn.js
+++ js/btn.js
@@ -1,11 +1,11 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -14,6 +14,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
