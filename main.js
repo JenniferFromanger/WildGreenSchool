f4c6bcfbb2de0881453fc8600cbd60ec59cea982
amjad <sammanamjad86@gmail.com>
2020-03-19 21:24:38 +0100

    add move img

--- main.js
+++ main.js
@@ -56,7 +56,12 @@ var year= new Date().getFullYear();
 var date = `© ${year} Wild Green School. Tous droits réservé`;
 document.getElementsByClassName('school-rights')[0].innerHTML = date;
 //---------------------//
+const el = document.querySelector("#module");
 
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
 
 
 
