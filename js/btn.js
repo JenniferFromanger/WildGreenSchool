4f974efbcfbd3057aeb56679aec213c4a82b2352
narudb <benjam.d@gmail.com>
2020-03-19 17:08:51 +0100

    Ajout du bouton top

--- js/btn.js
+++ js/btn.js
@@ -1,14 +1,14 @@
 //Get the button:
-mybutton = document.getElementById("myBtn");
+const myButton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
