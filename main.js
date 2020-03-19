d93fe43708dba13d6361b4413cc9344672a534ec
Aurelien <aurelien.badre.dev@gmail.com>
2020-03-19 12:48:58 +0100

    popup form

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function clickLink() {
 };
 
 burgerButton.addEventListener('click', clickLink);
+
+// popup contact
+
+const openModButton = document.getElementById('open-modal');
+const closeModButton = document.getElementById('close-modal');
+
+openModButton.addEventListener('click', (event)=> {
+    document.getElementById('modal').style.display = 'block';
+    event.preventDefault(); 
+    })
+    
+closeModButton.addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'none';
+    })
